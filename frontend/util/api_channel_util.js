88bd13820978d5b5310d17feac112ff6c58ccae2
Dustin A <dustin@aujero.net>
2019-03-29 22:31:46 -0400

    Added Channel and Message actions

---
+++ frontend/util/api_channel_util.js
@@ -0,0 +1,37 @@
+export const fetchChannels = (serverId) => (
+    $.ajax({
+        url: `/api/${serverId}/channels`,
+        method: 'GET',
+    })
+)
+
+export const fetchChannel = (id) => (
+    $.ajax({
+        url: `/api/channels/${id}`,
+        method: 'GET',
+    })
+)
+
+export const createChannel = (serverId, channel) => (
+    $.ajax({
+        url: `/api/${serverId}/channels`,
+        method: 'POST',
+        data: { channel }
+    })
+)
+
+export const updateChannel = (channel) => (
+    $.ajax({
+        url: `/api/channels/${channel.id}`,
+        method: 'PATCH',
+        data: { channel }
+    })
+)
+
+export const deleteChannel = (id) => (
+    $.ajax({
+        url: ``,
+        method: '',
+        data: {}
+    })
+)
---
+++ frontend/util/api_message_util.js
@@ -0,0 +1,39 @@
+export const fetchMessages = () => (
+    $.ajax({
+        url: ``,
+        method: '',
+        data: {}
+    })
+)
+
+export const fetchMessage = (id) => (
+    $.ajax({
+        url: ``,
+        method: '',
+        data: {}
+    })
+)
+
+export const createMessage = (message) => (
+    $.ajax({
+        url: ``,
+        method: '',
+        data: {}
+    })
+)
+
+export const updateMessage = (message) => (
+    $.ajax({
+        url: ``,
+        method: '',
+        data: {}
+    })
+)
+
+export const deleteMesssage = (id) => (
+    $.ajax({
+        url: ``,
+        method: '',
+        data: {}
+    })
+)
