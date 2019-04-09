250f5e5935b0b2708f342c8e54dd9ae73e358750
Dustin A <dustin@aujero.net>
2019-04-09 10:23:40 -0400

    Added user-server uniqueness validation

--- app/models/userserver.rb
+++ app/models/userserver.rb
@@ -1,6 +1,6 @@
 class Userserver < ApplicationRecord 
     validates :user_id, :server_id, presence: true 
-
+    validates :user_id, uniqueness: { scope: :server_id }
     belongs_to :user 
     belongs_to :server
 end
