979876bf71c3d099c91021e98c3e9d271b47a20f
Dustin A <dustin@aujero.net>
2019-04-09 17:32:24 -0400

    Commented MessageBroadcastJob

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,6 +5,6 @@ class Message < ApplicationRecord
     belongs_to :channel, class_name: :Channel, foreign_key: :channel_id 
     belongs_to :user,  class_name: :User, foreign_key: :user_id
 
-    after_create_commit { MessageBroadcastJob.perform_later(self)}
+    # after_create_commit { MessageBroadcastJob.perform_later(self)}
 
 end
