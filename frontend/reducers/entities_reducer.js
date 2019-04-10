95095ae031e4e51d961f03ef43ccde4b85204395
Dustin A <dustin@aujero.net>
2019-04-10 15:05:27 -0400

    Added users[reducer]

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import serversReducer from './entities/servers_reducer';
 import channelsReducer from './entities/channels_reducer';
+import usersReducer from './entities/users_reducer';
 
 export default combineReducers({
     servers: serversReducer,
-    channels: channelsReducer
+    channels: channelsReducer,
+    users: usersReducer
 });
