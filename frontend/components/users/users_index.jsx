a17404df6c6936a834cfa01aec8ef89f5554ff9b
Dustin A <dustin@aujero.net>
2019-04-10 15:53:17 -0400

    Initial UsersIndex

---
+++ frontend/components/users/users_index.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+
+const msp = (state, ownProps) => ({
+    currServerUsers: state.entities.servers[ownProps.match.params.serverId].users,
+    users: state.entities.users
+})
+
+class UsersIndex extends React.Component {
+
+
+    render() {
+        const users = this.props.currServerUsers.map( userId => {
+            return (
+                <li key={userId}>
+                    <div>{this.props.users[userId].username}</div>
+                </li>
+            )
+        })
+        return (
+            <>
+            <div className="user-header">Users</div>
+            <div className="user-main">
+                <ul className="user-ul">
+                    {users}
+                </ul>
+            </div>
+            </>
+        )
+    }
+}
+export default withRouter(connect(msp, null)(UsersIndex));
