beaba4668aa13b3a5b93fb6b0d978c58ea7c964a
Dustin A <dustin@aujero.net>
2019-04-10 15:07:27 -0400

    Initial users state

--- frontend/reducers/entities/users_reducer.js
+++ frontend/reducers/entities/users_reducer.js
@@ -1,4 +1,21 @@
 import { merge } from 'lodash';
 import { LOGIN_CURRENT_USER, LOGOUT_CURRENT_USER } from '../../actions/session_actions';
-import { RECEIVE_SERVERS }
+import { RECEIVE_SERVERS } from '../../actions/server_actions';
 
+
+const userReducer = (state = {}, action) => {
+    Object.freeze(state);
+    switch(action.type) {
+        case LOGIN_CURRENT_USER: {  
+            return merge({}, state, {[action.user.id]: action.user})
+        }
+        case LOGOUT_CURRENT_USER: {
+            return {};
+        }
+        case RECEIVE_SERVERS: {
+            return merge({}, state, action.payload.users)
+        }
+        default: return state;
+    }
+}
+export default userReducer;
