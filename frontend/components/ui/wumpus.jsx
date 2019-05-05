245cfe0c0fdc753055894fd35d3e2972ec470d7e
Dustin A <dustin@aujero.net>
2019-05-05 16:36:39 -0400

    Fixed 'class' warning

--- frontend/components/ui/wumpus.jsx
+++ frontend/components/ui/wumpus.jsx
@@ -9,13 +9,13 @@ const Wumpus = () => (
             Or 
             Checkout my Profiles: 
             <div>
-                <a href="https://www.linkedin.com/in/dustin-a-0aab33127/" target="_blank"><i class="fab fa-linkedin"></i> LinkedIn </a>
+                <a href="https://www.linkedin.com/in/dustin-a-0aab33127/" target="_blank"><i className="fab fa-linkedin"></i> LinkedIn </a>
                  - 
-                 <a href="https://github.com/dustinaujero" target="_blank"><i class="fab fa-github-square"></i> GitHub </a>
+                 <a href="https://github.com/dustinaujero" target="_blank"><i className="fab fa-github-square"></i> GitHub </a>
                  - 
-                 <a href="https://angel.co/dustin-aujero" target="_blank"><i class="fab fa-angellist"></i> AngelList </a>
+                 <a href="https://angel.co/dustin-aujero" target="_blank"><i className="fab fa-angellist"></i> AngelList </a>
                  - 
-                 <a href="http://dustin-aujero.me/" target="_blank"><i class="fas fa-user-alt"></i> PersonalSite </a>
+                 <a href="http://dustin-aujero.me/" target="_blank"><i className="fas fa-user-alt"></i> PersonalSite </a>
             </div>
         </div>
     </div>
